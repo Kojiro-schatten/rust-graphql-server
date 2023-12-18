19ee86c27782a53937bee161c286ada21ac33cd6
KojiroSchatten <banbirou@icloud.com>
2023-12-18 23:18:31 +0900

    feat: enumを対応してみた

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,66 @@
-use std::sync::Mutex;
+use std::sync::Mutex; //相互排他機能：複数のスレッドが同時にデータにアクセスすることを防ぐ。データ整合性を保つ
 
 use actix_web::web::Data;
 use actix_web::{guard, web, App, HttpResponse, HttpServer, Result};
 use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
-use async_graphql::{EmptySubscription, Object, Schema, SimpleObject};
+use async_graphql::{EmptySubscription, Enum, InputObject, Object, Schema, SimpleObject};
 use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
-use once_cell::sync::Lazy;
+use once_cell::sync::Lazy; //global dataや静的リソースを遅延初期化(データ呼び出し時まで初期化を遅らせる)する。
 
 // Query構造体を定義
 struct Query;
 
+// Lazyを使用してスレッドセーフ化することで、プログラムの実行開始時ではなく、
+// 最初にSEQUENCE, PHOTOSが使用されるときに初期化する。外部リソースへの接続など（時間がかかるものに有効）
 static SEQUENCE_ID: Lazy<Mutex<usize>> = Lazy::new(|| Mutex::new(0));
+// ||: クロージャ表現。空のベクタを初期値としている。複数スレッドからのアクセスを同期するために使用される。
 static PHOTOS: Lazy<Mutex<Vec<Photo>>> = Lazy::new(|| Mutex::new(vec![]));
 #[derive(SimpleObject, Clone)]
 struct Photo {
     id: usize,
     name: String,
     description: String,
+    category: PhotoCategory,
 }
 struct Mutation;
 
+#[derive(Enum, Copy, Clone, Eq, PartialEq)]
+enum PhotoCategory {
+    Selfie,
+    Portrailt,
+    Action,
+    Landscape,
+    Graphic,
+}
+
+impl Default for PhotoCategory {
+    fn default() -> Self {
+        PhotoCategory::Portrailt
+    }
+}
+#[derive(InputObject)]
+struct PostPhotoInput {
+    name: String,
+    description: String,
+    #[graphql(default_with = "PhotoCategory::default()")]
+    category: PhotoCategory,
+}
+
 #[Object]
 impl Mutation {
-    async fn post_photo(&self, name: String, description: String) -> Photo {
+    async fn post_photo(&self, input: PostPhotoInput) -> Photo {
         let mut id = SEQUENCE_ID.lock().unwrap();
         *id += 1;
         let photo = Photo {
             id: *id,
-            name,
-            description,
+            name: input.name,
+            description: input.description,
+            category: input.category,
         };
+        // PHOTOSグローバルMutext<Vec<Photo>>に新しいPhotoオブジェクトをpushする
+        // .lock(): このMutexをロックし、保持しているデータへのアクセスを試みる
+        // .unwrap(): ロックの取得が成功した場合、MutexGuardを取り出し、失敗した場合Panicを起こす
+        // .clone(): Photoオブジェクトのディープコピーを作成。オリジナルのphotoオブジェクトは関数呼び出し元に返却
         PHOTOS.lock().unwrap().push(photo.clone());
         photo
     }
